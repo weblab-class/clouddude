bdd7987634e7cfcc2dd0cc057a6c8ca37da82a4b
hgheiberger <harryh@mit.edu>
2022-01-11 14:55:05 -0500

    Upgrade Phaser

--- client/src/components/modules/Game.js
+++ client/src/components/modules/Game.js
@@ -1,15 +1,7 @@
 import React from "react";
-
-import Phaser from "phaser";
-
 import "./Game.css";
 
 const Game = () => {
-  // Initialize Game
-  window.onload = () => {
-    const game = Phaser.Game(1600, 900, Phaser.AUTO, "game");
-  };
-
   return <div id="gameContainer"> </div>;
 };
 
