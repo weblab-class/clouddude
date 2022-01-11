79e0515e0459ee2e98a0bea67e1ceb9d4db226f6
Henry Heiberger <henryrh@mit.edu>
2022-01-11 09:08:21 -0600

    Stlye fixes to index.js:

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.js";
+import App from "./components/App";
 
 // renders React Component "Root" into the DOM element with ID "root"
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // allows for live updating
