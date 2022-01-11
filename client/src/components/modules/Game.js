c8558809951ee154fee43183ccfc97048fc1097b
Henry Heiberger <henryrh@mit.edu>
2022-01-11 13:09:07 -0600

    Remove Phaser package

--- client/src/components/modules/Game.js
+++ client/src/components/modules/Game.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import Phaser from "phaser";
-
 import "./Game.css";
 
 const Game = () => {
   // Initialize Game
   window.onload = () => {
-    const game = Phaser.Game(1600, 900, Phaser.AUTO, "game");
+    //const game = Phaser.Game(1600, 900, Phaser.AUTO, "game");
   };
 
   return <div id="gameContainer"> </div>;
