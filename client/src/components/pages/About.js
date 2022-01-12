5b562a81f629a649725c187ca1f852649b801d86
Ashar Farooq <ashar744@gmail.com>
2022-01-12 02:10:12 -0500

    adding resource in about

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 
 import "../../utilities.css";
 import "./About.css";
 
 const About = () => {
   return (
-<>
+    <>
       <Helmet>
-          <title>About</title>
+        <title>About</title>
       </Helmet>
-</>
+
+      <h2>
+        Platformer is a web game where users can create levels with obstacles and platforms with the
+        goal of going from point A and point B.
+      </h2>
+
+      <p>
+        More information:
+        <iframe
+          title="About"
+          src="https://docs.google.com/presentation/d/e/2PACX-1vRTklKL7wdhlCN0fGXl0sECtT06LYDGAHpwCXC82zHYSg99lFI8hA-_izlpzDNzi33ZY75DDn8sj80V/embed?start=true&loop=true&delayms=3000"
+          frameBorder="0"
+          width="960"
+          height="569"
+          allowFullScreen="true"
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+        />
+      </p>
+    </>
   );
 };
 
