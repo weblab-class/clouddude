6c66de0a1916bc43cd56a06ed615a4b2ddc7b191
Ashar Farooq <ashar744@gmail.com>
2022-01-11 20:50:07 -0500

    basic levels state

--- client/src/components/pages/Repository.js
+++ client/src/components/pages/Repository.js
@@ -4,7 +4,65 @@ import "../../utilities.css";
 import "./Repository.css";
 
 const Repository = () => {
-  return <div />;
+  const [levels, setLevels] = useState([
+    {
+      creator: "Bob",
+      name: "Twisted",
+      start: { x: 20, y: 20 },
+      exit: { x: 40, y: 45 },
+      platforms: [
+        { image: "img1", x: 25, y: 20 },
+        { image: "img2", x: 30, y: 30 },
+      ],
+      decoration: [{ frame: 35, x: 35, y: 35 }],
+      coins: [
+        { x: 11, y: 40 },
+        { x: 12, y: 41 },
+      ],
+      obstacles: [
+        { type: "obs1", x: 40, y: 20 },
+        { type: "obs2", x: 41, y: 21 },
+      ],
+      funness: 9,
+      difficulty: 4,
+    },
+    {
+      creator: "Foo",
+      name: "Roller Coaster",
+      start: { x: 20, y: 20 },
+      exit: { x: 40, y: 45 },
+      platforms: [
+        { image: "img11", x: 25, y: 20 },
+        { image: "img22", x: 30, y: 30 },
+      ],
+      decoration: [{ frame: 35, x: 35, y: 35 }],
+      coins: [
+        { x: 11, y: 40 },
+        { x: 12, y: 41 },
+      ],
+      obstacles: [
+        { type: "obs11", x: 40, y: 20 },
+        { type: "obs22", x: 41, y: 21 },
+      ],
+      funness: 5,
+      difficulty: 9,
+    },
+  ]);
+
+  return (
+    <>
+      {levels.map((level) => {
+        return (
+          <div className="level-container">
+            <div className="level-name">{level.name}</div>
+            <div className="level-creator">{level.creator}</div>
+            <div className="level-funness">{level.funness}</div>
+            <div className="level-difficulty">{level.difficulty}</div>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Repository;
