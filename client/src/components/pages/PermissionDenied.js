c475b18585fcbe2ac4d4975ec2cb9c756813bbb8
Ashar Farooq <ashar744@gmail.com>
2022-01-15 16:48:15 -0500

    sign in required work

--- client/src/components/pages/PermissionDenied.js
+++ client/src/components/pages/PermissionDenied.js
@@ -5,12 +5,12 @@ import "./PermissionDenied.css";
 
 const PermissionDenied = () => {
   return (
-    <div>
+    <div className="main-container">
       <Helmet>
         <title>Sign In Required</title>
       </Helmet>
-      <h1>Sign-In Required</h1>
-      <p>Please sign in to design a level!</p>
+      <h1 className="main-error">Sign-In Required</h1>
+      <p className="err-message">Please Sign In to design a level!</p>
     </div>
   );
 };
