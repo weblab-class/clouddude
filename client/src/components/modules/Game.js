e43077cbb68099edf1fd9776983bd697e638263f
Henry Heiberger <henryrh@mit.edu>
2022-01-15 09:52:53 -0600

    Implement level editor gridpoint click recognitions

--- client/src/components/modules/Game.js
+++ client/src/components/modules/Game.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Phaser, { NONE } from "phaser";
 import "./Game.css";
 
 const Game = ({ editLevel, currentTool, activeLevel, isEditing }) => {
+  const [gridPoint, setGridPoint] = useState({ x: undefined, y: undefined });
+
   // Global configuration constants
   const gameWon = false;
   let isOver = false;
@@ -223,7 +225,10 @@ const Game = ({ editLevel, currentTool, activeLevel, isEditing }) => {
 
       grid.createMultiple({
         key: "grid",
+        name: "grid",
         frameQuantity: 576,
+        hitArea: new Phaser.Geom.Rectangle(1, 1, 49, 49),
+        hitAreaCallback: Phaser.Geom.Rectangle.Contains,
       });
 
       Phaser.Actions.GridAlign(grid.getChildren(), {
@@ -234,8 +239,9 @@ const Game = ({ editLevel, currentTool, activeLevel, isEditing }) => {
         x: 25,
         y: 25,
       });
-      Phaser.Actions.SetHitArea(grid.getChildren());
     }
+
+    this.input.on("gameobjectdown", clickCallback);
 
     // Handle platform collisions
     this.physics.add.collider(player, platforms);
@@ -287,6 +293,14 @@ const Game = ({ editLevel, currentTool, activeLevel, isEditing }) => {
     );
   }
 
+  const clickCallback = (pointer, gameObject) => {
+    setGridPoint({ x: gameObject.x, y: gameObject.y });
+  };
+
+  useEffect(() => {
+    editLevel(gridPoint);
+  }, [gridPoint]);
+
   function update() {
     // Responsively updates screen size
     responsivelyResize();
@@ -330,14 +344,16 @@ const Game = ({ editLevel, currentTool, activeLevel, isEditing }) => {
     }
   }
 
+  /*
   // Update click listener for current tool
   useEffect(() => {
-    const canvas = document.getElementById("game");
-    canvas.addEventListener("click", editLevel);
+    console.log(test);
+    savedInput.on("gameobjectdown", clickCallback);
     return () => {
-      canvas.removeEventListener("click", editLevel);
+      savedInput.off("gameobjectdown", clickCallback);
     };
   }, [currentTool, editLevel]);
+  */
 
   return (
     <div className="Game-container">
