a2b63e3eaa204ee09f0f4c522da47cb68827bc3d
hgheiberger <harryh@mit.edu>
2022-01-15 10:57:43 -0500

    Improve spike hitbox`

--- client/src/components/modules/Game.js
+++ client/src/components/modules/Game.js
@@ -59,6 +59,7 @@ const Game = ({ editLevel, currentTool, activeLevel, isEditing }) => {
         default: "arcade",
         arcade: {
           gravity: { y: 600 },
+          debug: true,
         },
       },
       scale: {
@@ -183,7 +184,10 @@ const Game = ({ editLevel, currentTool, activeLevel, isEditing }) => {
 
     for (const obstacle of sampleData.obstacles) {
       if (obstacle.type === "spike") {
-        spikes.create(obstacle.x, obstacle.y, "spike");
+        const spike = spikes.create(obstacle.x, obstacle.y, "spike");
+        spike.setScale(1.0, 0.75);
+        spike.y += 6.25;
+        spike.refreshBody();
       }
     }
 
