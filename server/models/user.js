cee8bfce5268c754cbdd484f1650d1f302cd6788
Ashar Farooq <ashar744@gmail.com>
2022-01-15 20:37:27 -0500

    changing user schema

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
+  levelsWon: Number,
+  levelsPublished: Number
 });
 
 // compile model from schema
