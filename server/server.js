5a2d7215154935ec17ef0f31d8cb565ddab8c50f
hgheiberger <harryh@mit.edu>
2022-01-17 14:43:43 -0500

    Change server host

--- server/server.js
+++ server/server.js
@@ -104,10 +104,12 @@ app.use((err, req, res, next) => {
 });
 
 // hardcode port to 3000 for now
-const port = process.env.PORT || 3000;
+const port = process.env.YOUR_PORT || process.env.PORT || 3000;
+const serverHost = process.env.YOUR_HOST || "0.0.0.0";
+
 const server = http.Server(app);
 socketManager.init(server);
 
-server.listen(port, () => {
+server.listen(port, serverHost, () => {
   console.log(`Server running on port: ${port}`);
 });
