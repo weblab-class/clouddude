f9173ae78f6b5059edf8e77803d66a2b1bb2f65d
Ashar Farooq <ashar744@gmail.com>
2022-01-22 15:17:11 -0500

    closing menu on outside click

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -38,6 +38,20 @@ const NavBar = ({
     setProfileModal(true);
   };
 
+  document.addEventListener("click", (event) => {
+    const clickElement = document.getElementById("document-page");
+    let targetElement = event.target;
+
+    do {
+      if (targetElement === clickElement) {
+        return;
+      }
+      targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    setMenuOpen(false);
+  });
+
   const iconClick = (event) => {
     setMenuOpen(true);
     setAnchorEl(event.currentTarget);
@@ -63,96 +77,98 @@ const NavBar = ({
   }, [userId]);
 
   return (
-    <nav className="NavBar-container">
-      <div className="NavBar-linkContainer u-inlineBlock">
-        <Link to="/" className="NavBar-link">
-          <img src="https://i.imgur.com/XTd5pLu.png" width="40" height="40" alt="Platformer" />
-        </Link>
-        <Link to="/about/" className="NavBar-link">
-          About
-        </Link>
-        <Link to="/repository/" className="NavBar-link">
-          Play
-        </Link>
-        <Link to="/leveleditor/" className="NavBar-link">
-          Design
-        </Link>
-      </div>
-      {userId ? (
-        <div className="login">
-          <IconButton
-            edge="end"
-            aria-label="current user"
-            aria-haspopup="true"
-            onClick={iconClick}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <div>
-            <Menu anchorEl={anchorEl} open={menuOpen} keepMounted>
-              <MenuItem onClick={handleProfile}>My Profile</MenuItem>
-              <MenuItem onClick={() => setMenuOpen(false)}>
-                <GoogleLogout
-                  clientId={GOOGLE_CLIENT_ID}
-                  buttonText="Logout"
-                  onLogoutSuccess={handleLogout}
-                  onFailure={(err) => console.log(err)}
-                />
-              </MenuItem>
-            </Menu>
-          </div>
-          <div>
-            <Modal show={profileModal} onHide={() => setProfileModal(false)}>
-              <Modal.Header>
-                <Modal.Title>
-                  {userName}
-                  's Profile
-                </Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <Form>
-                  <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                      onChange={(event) => setNewName(event.target.value)}
-                      type="text"
-                      placeholder="Edit Name"
-                      value={newName}
-                    />
-                  </Form.Group>
-
-                  <Form.Group className="mb-3" controlId="difficulty">
-                    <Form.Label>Published Levels: {publishedLevels}</Form.Label>
-                  </Form.Group>
-
-                  <Form.Group className="mb-3" controlId="funness">
-                    <Form.Label>Levels Won: {levelsWon}</Form.Label>
-                  </Form.Group>
-                </Form>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={() => setProfileModal(false)}>
-                  Close
-                </Button>
-                <Button variant="primary" onClick={handleSubmit}>
-                  Save
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </div>
+    <div id="document-page">
+      <nav className="NavBar-container">
+        <div className="NavBar-linkContainer u-inlineBlock">
+          <Link to="/" className="NavBar-link">
+            <img src="https://i.imgur.com/XTd5pLu.png" width="40" height="40" alt="Platformer" />
+          </Link>
+          <Link to="/about/" className="NavBar-link">
+            About
+          </Link>
+          <Link to="/repository/" className="NavBar-link">
+            Play
+          </Link>
+          <Link to="/leveleditor/" className="NavBar-link">
+            Design
+          </Link>
         </div>
-      ) : (
-        <div className="login">
-          <GoogleLogin
-            clientId={GOOGLE_CLIENT_ID}
-            buttonText="Login"
-            onSuccess={handleLogin}
-            onFailure={(err) => console.log(err)}
-          />
-        </div>
-      )}
-    </nav>
+        {userId ? (
+          <div className="login">
+            <IconButton
+              edge="end"
+              aria-label="current user"
+              aria-haspopup="true"
+              onClick={iconClick}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <div>
+              <Menu anchorEl={anchorEl} open={menuOpen} keepMounted>
+                <MenuItem onClick={handleProfile}>My Profile</MenuItem>
+                <MenuItem onClick={() => setMenuOpen(false)}>
+                  <GoogleLogout
+                    clientId={GOOGLE_CLIENT_ID}
+                    buttonText="Logout"
+                    onLogoutSuccess={handleLogout}
+                    onFailure={(err) => console.log(err)}
+                  />
+                </MenuItem>
+              </Menu>
+            </div>
+            <div>
+              <Modal show={profileModal} onHide={() => setProfileModal(false)}>
+                <Modal.Header>
+                  <Modal.Title>
+                    {userName}
+                    's Profile
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form>
+                    <Form.Group className="mb-3" controlId="name">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control
+                        onChange={(event) => setNewName(event.target.value)}
+                        type="text"
+                        placeholder="Edit Name"
+                        value={newName}
+                      />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="difficulty">
+                      <Form.Label>Published Levels: {publishedLevels}</Form.Label>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="funness">
+                      <Form.Label>Levels Won: {levelsWon}</Form.Label>
+                    </Form.Group>
+                  </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={() => setProfileModal(false)}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleSubmit}>
+                    Save
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </div>
+          </div>
+        ) : (
+          <div className="login">
+            <GoogleLogin
+              clientId={GOOGLE_CLIENT_ID}
+              buttonText="Login"
+              onSuccess={handleLogin}
+              onFailure={(err) => console.log(err)}
+            />
+          </div>
+        )}
+      </nav>
+    </div>
   );
 };
 
